856c0948df1bf383692a6d6d3b5678d06681fed8
legabog <probringer@mail.ru>
2020-05-08 00:05:28 +0300

    Add files via upload

---
+++ practice-js-legabog/05_array.js
@@ -0,0 +1,47 @@
+const cars = ["Ауди", "Форд", "Мерседес", "Бугатти"];
+const people = [
+  { name: "Oleg", budget: 4200 },
+  { name: "Kolya", budget: 3200 },
+  { name: "John", budget: 2200 },
+];
+const fib = [1, 1, 2, 3, 5, 8, 13];
+
+// cars.push("Ламба")
+// console.log(cars)
+// cars.unshift("Волга")
+// console.log(cars)
+// cars.shift()
+// console.log(cars)
+// cars.pop()
+// console.log(cars)
+
+// console.log(cars.reverse())
+
+// console.log(cars.indexOf("Форд"))
+
+// people.findIndex((e) => {
+//   if (e.budget === 3200) {
+//     console.log(e);
+//   }
+// });
+
+// for (let person of people) {
+//     if (person.budget === 3200) {
+//         console.log(person)
+//     }
+//     console.log(person)
+// }
+
+// console.log(cars.includes("Ауди"))
+
+// console.log(cars.map(car => car.toUpperCase()))
+// console.log(fib.map(e => e**2).map(Math.sqrt))
+// console.log(fib.map(e=> e**2).filter(e => e > 20))
+// console.log(people.filter(p => p.budget > 3000).reduce((acc, person) => acc += person.budget, 0))
+
+// Задача 1
+// function stringReverse(str) {
+//     return str.split("").reverse().join("")
+// }
+
+// console.log(stringReverse("lega"))
